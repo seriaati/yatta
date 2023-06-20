e9a01472a432ec4ef33ef4c0584b5a9250b978dc
seria_ati <seria.ati@gmail.com>
2023-06-20 12:12:45 +0800

    feat: add language enums

---
+++ enums.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class Language(Enum):
+    CHT = "cht"
+    CHS = "chs"
+    DE = "de"
+    EN = "en"
+    ES = "es"
+    FR = "fr"
+    ID = "id"
+    JP = "jp"
+    KR = "kr"
+    PT = "pt"
+    RU = "ru"
+    TH = "th"
+    VI = "vi"
