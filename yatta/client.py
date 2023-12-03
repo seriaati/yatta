456c797d310ac7b654224c432f02cbf2d3a89846
seriaati <seria.ati@gmail.com>
2023-12-03 19:32:35 +0800

    feat: add use_cache parameter to fetch functions

--- yatta/client.py
+++ yatta/client.py
@@ -1,16 +1,18 @@
+import asyncio
 import logging
 from enum import Enum
 from typing import Any, Dict, Final, List
 
 import aiohttp
+from diskcache import Cache
 
 from ..exceptions import DataNotFound
 from .models import (
     Book,
     BookDetail,
+    ChangeLog,
     Character,
     CharacterDetail,
-    ChangeLog,
     Item,
     ItemDetail,
     LightCone,
@@ -19,7 +21,6 @@ from .models import (
     RelicSet,
     RelicSetDetail,
 )
-
 
 __all__ = ("YattaAPI", "Language")
 
@@ -70,6 +71,9 @@ class YattaAPI:
     async def __aexit__(self, exc_type, exc, tb) -> None:
         await self.close()
 
+    async def _request(
+        self, endpoint: str, *, static: bool = False, use_cache: bool
+    ) -> Dict[str, Any]:
         """
         A helper function to make requests to the API.
 
@@ -79,6 +83,8 @@ class YattaAPI:
             The endpoint to request from.
         static: :class:`bool`
             Whether to use the static endpoint or not. Defaults to ``False``.
+        use_cache: :class:`bool`
+            Whether to use the cache or not
 
         Returns
         -------
@@ -101,6 +107,7 @@ class YattaAPI:
                 raise DataNotFound(data["data"])
             await asyncio.to_thread(self.cache.set, url, data, expire=86400)
             return data
+
     async def close(self) -> None:
         """
         Closes the client session and cache.
@@ -108,20 +115,25 @@ class YattaAPI:
         await self.session.close()
         self.cache.close()
 
-    async def fetch_books(self) -> List[Book]:
+    async def fetch_books(self, use_cache: bool = True) -> List[Book]:
         """
         Fetch all books from the API.
+
+        Parameters
+        ----------
+        use_cache: :class:`bool`, optional
+            Whether to use the cache or not. Defaults to ``True``.
 
         Returns
         -------
         List[Book]
             A list of Book objects.
         """
-        data = await self._request("book")
+        data = await self._request("book", use_cache=use_cache)
         books = [Book(**b) for b in data["data"]["items"].values()]
         return books
 
-    async def fetch_book_detail(self, id: int) -> BookDetail:
+    async def fetch_book_detail(self, id: int, use_cache: bool = True) -> BookDetail:
         """
         Fetch a book's detail from the API.
 
@@ -129,30 +141,39 @@ class YattaAPI:
         ----------
         id: :class:`int`
             The ID of the book to fetch.
+        use_cache: :class:`bool`, optional
+            Whether to use the cache or not. Defaults to ``True``.
 
         Returns
         -------
         BookDetail
             A BookDetail object.
         """
-        data = await self._request(f"book/{id}")
+        data = await self._request(f"book/{id}", use_cache=use_cache)
         book = BookDetail(**data["data"])
         return book
 
-    async def fetch_characters(self) -> List[Character]:
+    async def fetch_characters(self, use_cache: bool = True) -> List[Character]:
         """
         Fetch all characters from the API.
+
+        Parameters
+        ----------
+        use_cache: :class:`bool`, optional
+            Whether to use the cache or not. Defaults to ``True``.
 
         Returns
         -------
         List[Character]
             A list of Character objects.
         """
-        data = await self._request("avatar")
+        data = await self._request("avatar", use_cache=use_cache)
         characters = [Character(**c) for c in data["data"]["items"].values()]
         return characters
 
-    async def fetch_character_detail(self, id: int) -> CharacterDetail:
+    async def fetch_character_detail(
+        self, id: int, use_cache: bool = True
+    ) -> CharacterDetail:
         """
         Fetch a character's detail from the API.
 
@@ -160,30 +181,37 @@ class YattaAPI:
         ----------
         id: :class:`int`
             The ID of the character to fetch.
+        use_cache: :class:`bool`, optional
+            Whether to use the cache or not. Defaults to ``True``.
 
         Returns
         -------
         CharacterDetail
             A CharacterDetail object.
         """
-        data = await self._request(f"avatar/{id}")
+        data = await self._request(f"avatar/{id}", use_cache=use_cache)
         character = CharacterDetail(**data["data"])
         return character
 
-    async def fetch_items(self) -> List[Item]:
+    async def fetch_items(self, use_cache: bool = True) -> List[Item]:
         """
         Fetch all items from the API.
+
+        Parameters
+        ----------
+        use_cache: :class:`bool`, optional
+            Whether to use the cache or not. Defaults to ``True``.
 
         Returns
         -------
         List[Item]
             A list of Item objects.
         """
-        data = await self._request("item")
+        data = await self._request("item", use_cache=use_cache)
         items = [Item(**i) for i in data["data"]["items"].values()]
         return items
 
-    async def fetch_item_detail(self, id: int) -> ItemDetail:
+    async def fetch_item_detail(self, id: int, use_cache: bool = True) -> ItemDetail:
         """
         Fetch an item's detail from the API.
 
@@ -191,30 +219,39 @@ class YattaAPI:
         ----------
         id: :class:`int`
             The ID of the item to fetch.
+        use_cache: :class:`bool`, optional
+            Whether to use the cache or not. Defaults to ``True``.
 
         Returns
         -------
         ItemDetail
             An ItemDetail object.
         """
-        data = await self._request(f"item/{id}")
+        data = await self._request(f"item/{id}", use_cache=use_cache)
         item = ItemDetail(**data["data"])
         return item
 
-    async def fetch_light_cones(self) -> List[LightCone]:
+    async def fetch_light_cones(self, use_cache: bool = True) -> List[LightCone]:
         """
         Fetch all light cones from the API.
+
+        Parameters
+        ----------
+        use_cache: :class:`bool`, optional
+            Whether to use the cache or not. Defaults to ``True``.
 
         Returns
         -------
         List[LightCone]
             A list of LightCone objects.
         """
-        data = await self._request("equipment")
+        data = await self._request("equipment", use_cache=use_cache)
         light_cones = [LightCone(**lc) for lc in data["data"]["items"].values()]
         return light_cones
 
-    async def fetch_light_cone_detail(self, id: int) -> LightConeDetail:
+    async def fetch_light_cone_detail(
+        self, id: int, use_cache: bool = True
+    ) -> LightConeDetail:
         """
         Fetch a light cone's detail from the API.
 
@@ -222,55 +259,74 @@ class YattaAPI:
         ----------
         id: :class:`int`
             The ID of the light cone to fetch.
+        use_cache: :class:`bool`, optional
+            Whether to use the cache or not. Defaults to ``True``.
 
         Returns
         -------
         LightConeDetail
             A LightConeDetail object.
         """
-        data = await self._request(f"equipment/{id}")
+        data = await self._request(f"equipment/{id}", use_cache=use_cache)
         light_cone = LightConeDetail(**data["data"])
         return light_cone
 
-    async def fetch_messages(self) -> List[Message]:
+    async def fetch_messages(self, use_cache: bool = True) -> List[Message]:
         """
         Fetch all messages from the API.
+
+        Parameters
+        ----------
+        use_cache: :class:`bool`, optional
+            Whether to use the cache or not. Defaults to ``True``.
 
         Returns
         -------
         List[Message]
             A list of Message objects.
         """
-        data = await self._request("message")
+        data = await self._request("message", use_cache=use_cache)
         messages = [Message(**m) for m in data["data"]["items"].values()]
         return messages
 
-    async def fetch_message_types(self) -> Dict[str, str]:
+    async def fetch_message_types(self, use_cache: bool = True) -> Dict[str, str]:
         """
         Fetch all message types from the API.
+
+        Parameters
+        ----------
+        use_cache: :class:`bool`, optional
+            Whether to use the cache or not. Defaults to ``True``.
 
         Returns
         -------
         List[str]
             A list of message types.
         """
-        data = await self._request("message")
+        data = await self._request("message", use_cache=use_cache)
         return data["data"]["types"]
 
-    async def fetch_relic_sets(self) -> List[RelicSet]:
+    async def fetch_relic_sets(self, use_cache: bool = True) -> List[RelicSet]:
         """
         Fetch all relic sets from the API.
+
+        Parameters
+        ----------
+        use_cache: :class:`bool`, optional
+            Whether to use the cache or not. Defaults to ``True``.
 
         Returns
         -------
         List[RelicSet]
             A list of RelicSet objects.
         """
-        data = await self._request("relic")
+        data = await self._request("relic", use_cache=use_cache)
         relics = [RelicSet(**r) for r in data["data"]["items"].values()]
         return relics
 
-    async def fetch_relic_set_detail(self, id: int) -> RelicSetDetail:
+    async def fetch_relic_set_detail(
+        self, id: int, use_cache: bool = True
+    ) -> RelicSetDetail:
         """
         Fetch a relic set's detail from the API.
 
@@ -278,6 +334,8 @@ class YattaAPI:
         ----------
         id: :class:`int`
             The ID of the relic set to fetch.
+        use_cache: :class:`bool`, optional
+            Whether to use the cache or not. Defaults to ``True``.
 
         Returns
         -------
@@ -285,20 +343,25 @@ class YattaAPI:
             A RelicSetDetail object.
         """
         logging.debug(f"Fetching relic set detail for ID {id}")
-        data = await self._request(f"relic/{id}")
+        data = await self._request(f"relic/{id}", use_cache=use_cache)
         relic = RelicSetDetail(**data["data"])
         return relic
 
-    async def fetch_change_logs(self) -> List[ChangeLog]:
+    async def fetch_change_logs(self, use_cache: bool = True) -> List[ChangeLog]:
         """
         Fetch change logs from the API.
+
+        Parameters
+        ----------
+        use_cache: :class:`bool`, optional
+            Whether to use the cache or not. Defaults to ``True``.
 
         Returns
         -------
         List[ChangeLog]
             A list of ChangeLog objects.
         """
-        data = await self._request("changelog", static=True)
+        data = await self._request("changelog", static=True, use_cache=use_cache)
         change_logs: List[ChangeLog] = []
         for id, log in data["data"].items():
             change_logs.append(ChangeLog(id=int(id), **log))
